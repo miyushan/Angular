bd876354a12f18970339edc84ef29f5d9f86b037
Miyushan <smiushan2015@gmail.com>
2020-08-31 19:57:39 +0530

    Assignment- 1 completed

---
+++ src/app/shared/dishsecond.ts
@@ -0,0 +1,18 @@
+export class Dishsecond{
+    id: string;
+    name: string;
+    image: string;
+    category: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    description: string;
+    comments:{
+        comment: string;
+        rating: number;
+        date: string;
+        author: string;
+    };
+
+
+}
