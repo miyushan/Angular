021ec519f8113a3eb1c88bf62a7e35e407d79ffc
Miyushan <smiushan2015@gmail.com>
2020-09-02 16:52:10 +0530

    Header and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,11 +17,16 @@ import { from } from 'rxjs';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
